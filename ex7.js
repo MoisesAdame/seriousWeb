dc2a22ab0c77c530a2ffbf8a26217013061c354d
MoisesAdame <moisesadame01@gmail.com>
2023-01-03 02:00:40 -0600

    functioning api clock, setTimeout in order to reduce the number of calls

---
+++ ex7.js
@@ -0,0 +1,29 @@
+// Author: Moisés Adame-Aguilar
+// Date: January 3, 2023
+// Descrption: Api Calls & Ajax
+
+function getTime(){
+    var request = new XMLHttpRequest()
+
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200){
+            var date = JSON.parse(request.responseText)
+
+            date = new Date(date.utc_datetime)
+
+            addToParagraph(date)
+
+            getTime()
+        }else if(request.readyState === 4){
+            alert('Communication error: No data received!')
+        }
+    })
+    
+    request.open('GET', 'http://worldtimeapi.org/api/timezone/America/Mexico_City')
+    request.send()
+}
+
+function addToParagraph(date){
+    document.getElementById('time-paragraph').innerHTML = ''
+    document.getElementById('time-paragraph').innerHTML = date
+}
