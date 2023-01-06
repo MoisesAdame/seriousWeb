952550938fc28cb2ac4191c73a9bcd60cb100f86
MoisesAdame <moisesadame01@gmail.com>
2023-01-05 21:55:55 -0600

    async & await, not done...

---
+++ ex14.js
@@ -0,0 +1,25 @@
+// Author: Moisés Adame-Aguilar
+// Date: January 5, 2023
+// Descrption: Deep down in Ajax... Async & Await
+
+// Async: Makes a function return a Promise.
+// Await: Makes a function wait for a Promise.
+
+// -- Example 1: The following functions are the same.
+async function myFunction1() {
+    return "Hello";
+}
+
+function myFunction2(){
+    return Promise.resolve('Hello')
+}
+
+// Using the promise
+myFunction1().then(
+    function(value){
+        console.log(value)
+    }
+)
+
+// -- Example 2: Await can only be used in async functions
+// It makes the function pause til' the promise gets resolved.
